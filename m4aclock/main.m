cefe04ef199ba49c855b6471caa383151b317637
Harry Jones <i.am@harryj.co.uk>
2017-01-11 15:05:23 +0000

    Initial Commit

---
+++ m4aclock/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  m4aclock
+//
+//  Created by Harry Jones on 11/01/2017.
+//  Copyright © 2017 Harry Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
